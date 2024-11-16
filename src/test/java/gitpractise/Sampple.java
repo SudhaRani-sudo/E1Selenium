854c5aff83fae6e9ba00737c89e8c79a707b6d60
sudha <sudha@SudhaRani>
2024-11-16 08:56:42 +0530

    New Class added

---
+++ src/test/java/gitpractise/Sampple.java
@@ -0,0 +1,12 @@
+package gitpractise;
+
+import org.testng.annotations.Test;
+
+public class Sampple {
+	@Test
+	public void  practise() {
+		System.out.println("one");
+		System.out.println("two");
+
+}
+}

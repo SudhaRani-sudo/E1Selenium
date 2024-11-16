41f8785f8e3d7c7da989946a86b2e16497a00d97
sudha <sudha@SudhaRani>
2024-11-16 09:49:51 +0530

    Second Class

---
+++ src/test/java/gitpractise/Hllo.java
@@ -0,0 +1,12 @@
+package gitpractise;
+
+import org.testng.annotations.Test;
+
+public class Hllo {
+	@Test
+	public void  practise() {
+		System.out.println("three");
+		System.out.println("tfour");
+
+}
+}
